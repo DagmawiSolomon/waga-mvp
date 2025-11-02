94016dd6f77de1d158a002f394cfbcd80862784f
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-11-02 22:03:05 +0300

    style:

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -123,7 +123,7 @@ export default function HeroSection() {
           </div>
         </section>
 
-        <section className="relative">
+        <section className="relative pt-2">
        
            <Features />
             <HowItWorks/> 
